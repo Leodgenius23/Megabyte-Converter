f12904883e31d4fc0d72c8143de9f61a7ee89c68
Leodgenius <leonisnmassaquoi234@gmail.com>
2022-01-04 23:31:05 +0000

    Decimal update

--- src/com/company/DecimalComparator.java
+++ src/com/company/DecimalComparator.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double x, double y) {
+         x = (int)(x * 1000);
+         y = (int)(y * 1000);
+         if(x == y){
+             return true;
+         }
+         return false;
+    }
+
 }
+
+
+
