a5b01695c97d266491df33b79f892b4704c99008
Leodgenius <leonisnmassaquoi234@gmail.com>
2022-01-06 01:45:21 +0000

    new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,23 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-//      boolean result = LeapYear.isLeapYear(1924);
-        boolean result = TeenNumberchecker.hasTeen(23, 15, 42);
-        boolean result2 = TeenNumberchecker.isTeen(13);
-        System.out.println(result);
-        System.out.println(result2);
+        double areaOfCircle = AreaCalculator.area(5.0);
+        double areaOfCircle2 = AreaCalculator.area(-1);
+        double areaOfRectangle = AreaCalculator.area(5.0,4.0);
+        double areaOfRectangle2 = AreaCalculator.area(-1.0,4.0);
+        System.out.println("Area of circle = " + String.format("%.5f",areaOfCircle));
+        System.out.println("Area of circle = " + String.format("%.2f",areaOfCircle2));
+        System.out.println("Area of rectangle= " + String.format("%.2f",areaOfRectangle));
+        System.out.println("Area of rectangle = " + String.format("%.2f",areaOfRectangle2));
+
+
+
+
+
+////      boolean result = LeapYear.isLeapYear(1924);
+//        boolean result = TeenNumberchecker.hasTeen(23, 15, 42);
+//        boolean result2 = TeenNumberchecker.isTeen(13);
+//        System.out.println(result);
+//        System.out.println(result2);
     }
 }
