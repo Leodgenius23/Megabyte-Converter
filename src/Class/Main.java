2fc03ebea03da2c38f33507ca82ec1641f617cb2
Leodgenius <leonisnmassaquoi234@gmail.com>
2022-01-06 01:03:10 +0000

    all

--- src/Class/Main.java
+++ src/Class/Main.java
@@ -2,27 +2,42 @@ package Class;
 
 public class Main {
     public static void main(String[] args){
-//        int newScore = calculateScore("Tim",500);
-//        System.out.println("New score is " + newScore);
-//        calculateScore(75);
-//        calculateScore();
-        CalcFeetAndInches.calcFeetAndInchesToCentimeters(6,0);
-        CalcFeetAndInches.calcFeetAndInchesToCentimeters(157);
-    }
-//    public static int calculateScore(String playerName, int score){
-//        System.out.println("Player " + playerName +
-//                " scored " + score + " points");
-//        return score * 1000;
-//    }
+        System.out.println(getDurationString(65L,45L));
+        System.out.println(getDurationString(3945L));
 
-//    public static int calculateScore(int score){
-//        System.out.println("Unnamed player scored " +
-//                score + " points");
-//        return score * 1000;
-//    }
-//
-//    public static int calculateScore(){
-//        System.out.println("No player name, no player score. ");
-//        return 0;
-//    }
+    }
+    public static String getDurationString(long minutes, long seconds){
+        if(minutes >= 0){
+            if(seconds >= 0 && seconds <= 59){
+                long hours = minutes / 60;
+                long remainingMinutes = minutes % 60;
+
+                String hourString = hours + "h";
+                if(hours < 10){
+                    hourString = "0" + hourString;
+                }
+
+                String minutesString = remainingMinutes + "h";
+                if(remainingMinutes < 10){
+                    minutesString = "0" + minutesString;
+                }
+
+                String secondString = seconds + "h";
+                if(seconds < 10){
+                    secondString = "0" + secondString;
+                }
+                return hourString + " " + minutesString + " " + secondString + "s";
+            }
+        }
+       return "Invalid value";
+        }
+
+    public static String getDurationString(long seconds){
+        if(seconds >= 0){
+            long minutes = seconds / 60;
+            long remainingSeconds = seconds % 60;
+            return getDurationString(minutes, remainingSeconds);
+        }
+        return "Invalid value";
+    }
 }
