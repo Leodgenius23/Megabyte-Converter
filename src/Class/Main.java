fa9e46db418410f3bd35f6f5419297cd1412b893
Leodgenius <leonisnmassaquoi234@gmail.com>
2022-01-17 00:57:02 +0000

    Wall Exercises

--- src/Class/Main.java
+++ src/Class/Main.java
@@ -26,17 +26,32 @@ public class Main {
 //        return sum;
 //    }
     public static void main(String[] args) {
-      SimpleCalculator calculator = new SimpleCalculator();
-      calculator.setFirstNumber(5.0);
-      calculator.setSecondNumber(4);
+      Wall wall = new Wall(5,4);
+      System.out.println("Area = " + wall.getArea());
 
-        System.out.println("Add = " + calculator.getAdditionResult());
-        System.out.println("Subtraction = " + calculator.getSubtractionResult());
 
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-        System.out.println("Multiply = " + calculator.getMultiplicationResult());
-        System.out.println("Divide = " + calculator.getDivisionResult());
+
+
+
+
+
+
+
+
+
+
+
+//      SimpleCalculator calculator = new SimpleCalculator();
+//      calculator.setFirstNumber(5.0);
+//      calculator.setSecondNumber(4);
+//
+//        System.out.println("Add = " + calculator.getAdditionResult());
+//        System.out.println("Subtraction = " + calculator.getSubtractionResult());
+//
+//        calculator.setFirstNumber(5.25);
+//        calculator.setSecondNumber(0);
+//        System.out.println("Multiply = " + calculator.getMultiplicationResult());
+//        System.out.println("Divide = " + calculator.getDivisionResult());
 
 
 
